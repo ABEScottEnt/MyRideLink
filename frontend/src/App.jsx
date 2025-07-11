0c3a049462fc0e3d89a2926eb801ed8964a5d766
Justin <justinavodroc@gmail.com>
2025-07-11 11:50:05 -0400

    login-->auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home.jsx";
-import Login from "./pages/Login.jsx";
+import Login from "./pages/Auth.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 
