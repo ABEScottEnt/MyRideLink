f6f72ab3b568f09d82d57252d2a00d10238e3834
sanjana-patel226 <spatel226@hawk.iit.edu>
2025-07-18 16:02:21 -0500

    Added a new ride comparison section

--- frontend/app/home/(tabs)/rides.jsx
+++ frontend/app/home/(tabs)/rides.jsx
@@ -1,33 +1,48 @@
-import { View, Text, StyleSheet } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import SectionTitle from '../../../components/common/SectionTitle'
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import SectionTitle from "../../../components/common/SectionTitle";
+import RideComparisonList from "../../../components/rides/RideComparisonList";
+import { uberData, lyftData } from "../../../components/rides/testRides";
 
 export default function Rides() {
   return (
     <SafeAreaView style={styles.safe}>
-      <View style={styles.container}>
-        <SectionTitle title="Rides" />
-        <Text style={styles.subtitle}>Compare rides.</Text>
-      </View>
+      <ScrollView className="flex-1 bg-gray-50">
+        <View style={styles.container}>
+          <SectionTitle title="Rides" />
+          <Text
+            style={{
+              fontSize: 20,
+              textAlign: "center",
+              fontWeight: "bold",
+            }}
+          >
+            Available Rides
+          </Text>
+        </View>
+
+        <RideComparisonList title="Uber" eta="2-5 min" rides={uberData} />
+        <RideComparisonList title="Lyft" eta="3-6 min" rides={lyftData} />
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   safe: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   container: {
     flex: 1,
-    justifyContent: 'center',     // ⬅️ Center vertically (optional)
-    alignItems: 'center',         // ⬅️ Center everything horizontally
+    justifyContent: "center", // ⬅️ Center vertically (optional)
+    alignItems: "center", // ⬅️ Center everything horizontally
     paddingHorizontal: 16,
   },
   subtitle: {
     fontSize: 16,
-    color: '#666',
+    color: "#666",
     marginTop: 8,
-    textAlign: 'center',          // ⬅️ Center the text inside Text
+    textAlign: "center", // ⬅️ Center the text inside Text
   },
-})
+});
