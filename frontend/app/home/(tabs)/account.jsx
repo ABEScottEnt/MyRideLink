2dfb073d1652d317b25c7ac67a2d0a774dfdca88
Sainath57 <sainath22042002@gmail.com>
2025-07-23 15:35:11 -0400

    Updated Accounts page

--- frontend/app/home/(tabs)/account.jsx
+++ frontend/app/home/(tabs)/account.jsx
@@ -1,12 +1,39 @@
-import COLORS from "@/constants/theme";
-import { Text, View } from "react-native";
+import {View, StyleSheet, ScrollView} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react';
+import SectionTitle from "../../../components/common/SectionTitle";
+import RecentActivity from "../../../components/home/RecentActivity";
 import SettingCard from "../../../components/account/SettingCard";
+import ProfileCard from "../../../components/account/ProfileCard";
+import StatsCard from "../../../components/account/StatsCard";
 
-export default function Home() {
-  return (
-    <View style={{ backgroundColor: COLORS.background }}>
-      <Text>Account</Text>
-      <SettingCard />
-    </View>
-  );
+
+export default function Account() {
+    return (
+        <SafeAreaView style={styles.safe}>
+            <ScrollView>
+                <View style={styles.container}>
+                    <SectionTitle
+                        title="Account"
+                        subtitle="Manage your profile and preferences"
+                    />
+                    <ProfileCard/>
+                    <StatsCard/>
+                    <RecentActivity/>
+                    <SettingCard/>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    safe: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    container: {
+        width: "90%",
+        alignSelf: "center",
+    },
+})
---
+++ frontend/components/account/ProfileCard.jsx
@@ -0,0 +1,114 @@
+import {View, Text, StyleSheet, TouchableOpacity, Alert} from "react-native";
+import {Box} from "lucide-react-native";
+import {Ionicons} from "@expo/vector-icons";
+import COLORS from "../../constants/theme";
+
+export default function ProfileCard(){
+
+    //Call all the Info from backend here
+    const picture = ""
+    const firstName = "John"
+    const middleName = ""
+    const lastName = "Doe"
+    const email = "john.doe@xyz.com"
+    const subscription = true
+    const rating = "5"
+
+    let profilePic;
+    if(picture){
+        profilePic = picture;
+    }
+    else {
+        profilePic = <Ionicons name="person-circle-outline" size={60} color="blue" style={styles.picHolder}/>
+    }
+
+    const fullName = middleName
+        ?`${firstName} ${middleName} ${lastName}`
+        :`${firstName} ${lastName}`;
+
+    let emailAddress;
+    if (!email) {
+        reportError("Email address required");
+    } else {
+         emailAddress = `${email}`;
+    }
+
+    let subscriptionTier;
+    if (!subscription) {
+        subscriptionTier = "Free";
+    }
+    else {
+        subscriptionTier = "Pro";
+    }
+
+
+    return (
+        <View style={styles.profileContainer}>
+            <View style={styles.profile}>
+                {profilePic}
+                <View style={styles.details}>
+                    <Text style={styles.fullName}>{fullName}</Text>
+                    <Text style={styles.email}>{emailAddress}</Text>
+                </View>
+            </View>
+            <View style={styles.buttons}>
+                <TouchableOpacity onPress={() => Alert.alert(`You have ${subscriptionTier} subscription`)} style={styles.buttonContainer}><Text style={styles.subscriptionTier}>{subscriptionTier}</Text></TouchableOpacity>
+                <TouchableOpacity onPress={() => Alert.alert(`You have ${rating} ⭐ rating`)} style={styles.buttonContainer}><Text style={styles.rating}>{rating} ⭐</Text></TouchableOpacity>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    profileContainer: {
+        borderColor: COLORS.muted,
+        borderWidth: 1,
+        borderRadius: 12,
+        padding: 12,
+    },
+    profile: {
+        flexDirection: "row",
+    },
+    details: {
+        flexDirection: "column",
+        marginLeft: 10,
+        marginTop: 8
+    },
+    picHolder: {
+
+    },
+    fullName: {
+        fontWeight: "bold",
+        fontSize: 18,
+    },
+    email: {
+        fontWeight: "italic",
+    },
+    buttons:{
+        flexDirection: "row",
+        marginLeft: 60
+    },
+    buttonContainer: {
+        //backgroundColor: '#cf4d4d',
+        padding: 16,
+        //backgroundColor: COLORS.primary,
+        borderColor: COLORS.primary,
+        borderWidth: 1,
+        borderRadius: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 6,
+        height: 60,
+        //width: 70,
+    },
+    subscriptionTier: {
+        color: COLORS.muted,
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    rating: {
+        color: COLORS.muted,
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+})
